2beefcbc21b8fa56b892b85ef951fb1e317b424a
anttibull <andriibu@wix.com>
2022-08-30 20:23:03 +0300

    feat: fetch data

---
+++ src/App/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, FC, useState, useCallback } from "react";
+
+import axios from "axios";
+
+import { Card } from "../components/Card/Card";
+import {  ChuckNorrisSearchResponseType } from "../types/api.types";
+
+
+
+
+export const App: FC = () => {
+
+  const [data, setData] = useState<ChuckNorrisSearchResponseType | null>(null)
+
+  const fetchRandomData = useCallback(async () => {
+      const {data} = await axios.get<ChuckNorrisSearchResponseType>('https://api.chucknorris.io/jokes/search?query=test')
+
+      setData(data)
+    },[])
+
+  useEffect(() => {
+    fetchRandomData()
+  }, [fetchRandomData])
+
+  if(!data) return <h1>Rusni pizda</h1>
+
+  return <>
+     {data.result.map((data) => (<Card icon={data.icon_url} text={data.value} key={data.id}/>))}
+  </>
+}
---
+++ src/types/api.types.ts
@@ -0,0 +1,11 @@
+export type ChuckNorrisResponseType = {
+    icon_url: string
+    id: string
+    url: string
+    value: string
+}
+
+export type ChuckNorrisSearchResponseType = {
+   total: number
+   result: Array<ChuckNorrisResponseType>
+}
